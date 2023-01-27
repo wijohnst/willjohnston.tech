be45ce6f601bf5608cfae727af4bf4a890dcabf2
wijohnst <wijohnst@gmail.com>
2023-01-26 19:22:41 -0500

    Updates configs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  // Type check TypeScript files
-  '**/*.(ts|tsx)': () => 'npm run tsc --noEmit',
+  // this will check Typescript files
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
-  // Lint & Prettify TS and JS files
+  // This will lint and format TypeScript and                                             //JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `npm run eslint ${filenames.join(' ')}`,
-    `npm run prettier --write ${filenames.join(' ')}`,
+    `yarn eslint --fix ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
   ],
 
-  // Prettify only Markdown and JSON files
+  // this will Format MarkDown and JSON
   '**/*.(md|json)': (filenames) =>
-    `npm run prettier --write ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
 }
