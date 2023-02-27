e948a8405986e1046896a0ccfb3fca62fc8aed12
wijohnst <wijohnst@gmail.com>
2023-02-27 16:15:31 -0700

    fix: Updates favicon path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <title>willjohnston.tech</title>
         <meta name="description" content="Full Stack Web Development" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
         <span>WillJohnston.tech</span>
