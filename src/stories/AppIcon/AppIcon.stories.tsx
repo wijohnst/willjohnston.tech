517e386b1d43335821d4e94a9c4d8728ae24cafa
wijohnst <wijohnst@gmail.com>
2023-03-03 19:48:56 -0500

    Adds a11y stuff to icons

--- src/stories/AppIcon/AppIcon.stories.tsx
+++ src/stories/AppIcon/AppIcon.stories.tsx
@@ -8,9 +8,21 @@ import CodeIcon from '../../../public/Icons/code-icon.svg';
 export default {
   title: 'Shared/AppIcon',
   component: AppIcon,
-  decorators: [(Story) => <Story />],
+  decorators: [
+    (Story) => (
+      <div style={{ width: '75px' }}>
+        <Story />
+      </div>
+    ),
+  ],
 } as ComponentMeta<typeof AppIcon>;
 
 export const Default: ComponentStory<typeof AppIcon> = (args) => (
   <AppIcon {...args}></AppIcon>
+);
+
+export const CustomIcon: ComponentStory<typeof AppIcon> = (args) => (
+  <AppIcon {...args}>
+    <CodeIcon role="image" alt="WillJohnston.tech Logo" />
+  </AppIcon>
 );
--- src/stories/AppIcon/AppIcon.tsx
+++ src/stories/AppIcon/AppIcon.tsx
@@ -3,12 +3,19 @@ import * as React from 'react';
 import MissingIcon from '../../../public/Icons/missing-icon.svg';
 
 type Props = {
+  /** What should be displayed? */
   children?: React.ReactElement;
 };
 
 const AppIcon = ({ children }: Props): React.ReactElement => {
   return (
-    <div data-testid="app-icon">{children ? children : <MissingIcon />}</div>
+    <div data-testid="app-icon">
+      {children ? (
+        children
+      ) : (
+        <MissingIcon role="image" alt="Question mark icon" />
+      )}
+    </div>
   );
 };
 
