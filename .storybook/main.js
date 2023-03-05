902b91d20421907f585e319ea9574a4ff02b1483
wijohnst <wijohnst@gmail.com>
2023-03-04 22:03:29 -0500

    Converts Storybook to Next.js Framework

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,38 +1,37 @@
 const path = require('path');
-
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
   },
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, '../src'),
     };
+
     const imageRule = config.module?.rules?.find((rule) => {
       const test = rule.test;
-
       if (!test) {
         return false;
       }
-
       return test.test('.svg');
     });
-
     imageRule.exclude = /\.svg$/;
-
     config.module?.rules?.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-
     return config;
+  },
+  docs: {
+    autodocs: true,
   },
 };
