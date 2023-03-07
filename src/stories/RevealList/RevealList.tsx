e97bd70d9724077d2892fc553d13a8b4c0c617a9
wijohnst <wijohnst@gmail.com>
2023-03-06 19:15:41 -0500

    Refactors component to use overlays instead of flexbox

--- src/stories/RevealList/RevealList.tsx
+++ src/stories/RevealList/RevealList.tsx
@@ -4,72 +4,37 @@ import {
   SemanticElement,
   RevealSection,
 } from '@/stories/RevealList/RevealList.style';
-import { getHeadingString } from '@/stories/RevealList/RevealList.utils';
+import {
+  defaultRevealListContent,
+  getHeadingString,
+  RevealListContent,
+  RevealListItem,
+} from '@/stories/RevealList/RevealList.utils';
 
 type Props = {
   activeHeading: string | undefined;
+  revealListContent?: RevealListContent;
 };
-const RevealList = ({ activeHeading }: Props): React.ReactElement => {
+const RevealList = ({
+  activeHeading,
+  revealListContent = defaultRevealListContent,
+}: Props): React.ReactElement => {
   return (
     <SemanticElement data-testid="reveal-list">
-      <RevealSection>
-        <div className="heading-wrapper">
-          <h1>
-            {getHeadingString(
-              'webdev',
-              activeHeading === 'webdev' ? 'half' : 'full',
-            )}
-          </h1>
-        </div>
-        {activeHeading === 'webdev' && (
-          <div className="reveal-content">CONTENT</div>
-        )}
-      </RevealSection>
-      <RevealSection>
-        {activeHeading === 'mentorship' && (
-          <div className="reveal-content">CONTENT</div>
-        )}
-        <div className="heading-wrapper">
-          <h1>
-            {getHeadingString(
-              'mentorship',
-              activeHeading === 'mentorship' ? 'half' : 'full',
-            )}
-          </h1>
-        </div>
-      </RevealSection>
-      <RevealSection>
-        {activeHeading === 'consulting' && (
-          <div className="reveal-content">CONTENT</div>
-        )}
-        <div className="heading-wrapper">
-          <h1>
-            {getHeadingString(
-              'consulting',
-              activeHeading === 'consulting' ? 'half' : 'full',
-            )}
-          </h1>
-        </div>
-        {activeHeading === 'consulting' && (
-          <div className="reveal-content">CONTENT</div>
-        )}
-      </RevealSection>
-      <RevealSection>
-        {activeHeading === 'consulting' && (
-          <div className="reveal-content">CONTENT</div>
-        )}
-        <div className="heading-wrapper">
-          <h1>
-            {getHeadingString(
-              'freelance',
-              activeHeading === 'freelance' ? 'half' : 'full',
-            )}
-          </h1>
-        </div>
-        {activeHeading === 'freelance' && (
-          <div className="reveal-content">CONTENT</div>
-        )}
-      </RevealSection>
+      {revealListContent.map(({ heading, underlayContent }: RevealListItem) => {
+        const isActive = heading === activeHeading;
+        return (
+          <RevealSection key={heading} isActive={isActive}>
+            <div className="overlay">
+              <h1>{getHeadingString(heading, isActive ? 'half' : 'full')}</h1>
+            </div>
+            <div className="underlay">
+              <div className="underlay-left"></div>
+              <div className="underlay-right">{underlayContent}</div>
+            </div>
+          </RevealSection>
+        );
+      })}
     </SemanticElement>
   );
 };
