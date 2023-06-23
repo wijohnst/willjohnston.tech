f986d10ad88a5de250dee4356d02434fd9c7595e
Will Johnston <will.johnston@kroger.com>
2023-06-23 11:06:28 -0400

    Refactors icon.

--- src/stories/NavBar/NavBar.tsx
+++ src/stories/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useRouter } from 'next/router';
 
 import { AppIcon } from '../AppIcon/AppIcon';
-import { Nav, IconWrapper } from './NavBar.style';
+import { SemanticElement, IconWrapper } from './NavBar.style';
 import CodeIcon from '../../../public/Icons/code-icon.svg';
 import { NavButton } from '@/stories/NavButton/NavButton';
 
@@ -12,35 +12,33 @@ const NavBar = ({}: Props): React.ReactElement => {
   const router = useRouter();
 
   return (
-    <Nav>
-      <>
-        <NavButton
-          pathname="About"
-          buttonText="about"
-          isActive={router.pathname === '/About'}
-        />
-        <NavButton
-          pathname="Portfolio"
-          buttonText="portfolio"
-          isActive={router.pathname === '/Portfolio'}
-        />
-        <IconWrapper>
-          <AppIcon handleClick={() => router.push('/')}>
-            <CodeIcon />
-          </AppIcon>
-        </IconWrapper>
-        <NavButton
-          pathname="Services"
-          buttonText="services"
-          isActive={router.pathname === '/Services'}
-        />
-        <NavButton
-          pathname="blog/"
-          buttonText="blog"
-          isActive={router.pathname === '/Blog'}
-        />
-      </>
-    </Nav>
+    <SemanticElement>
+      <NavButton
+        pathname="About"
+        buttonText="about"
+        isActive={router.pathname === '/About'}
+      />
+      <NavButton
+        pathname="Portfolio"
+        buttonText="portfolio"
+        isActive={router.pathname === '/Portfolio'}
+      />
+      <IconWrapper>
+        <AppIcon handleClick={() => router.push('/')}>
+          <CodeIcon />
+        </AppIcon>
+      </IconWrapper>
+      <NavButton
+        pathname="Services"
+        buttonText="services"
+        isActive={router.pathname === '/Services'}
+      />
+      <NavButton
+        pathname="blog/"
+        buttonText="blog"
+        isActive={router.pathname === '/Blog'}
+      />
+    </SemanticElement>
   );
 };
 
