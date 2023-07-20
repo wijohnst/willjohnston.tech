c0ad2861aeb49c01a1c0f331da8e3cf7daf979de
Will Johnston <will.johnston@kroger.com>
2023-07-20 13:32:08 -0400

    Adds hamburger menu

---
+++ src/stories/HamburgerMenu/HamburgerMenu.style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SemanticElement = styled.div`
+  .nav-reveal {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+
+  .close-button-wrapper {
+    width: 25px;
+  }
+`;
