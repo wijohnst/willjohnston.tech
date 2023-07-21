c0a7a63a85c819c4b1044cd7d60e4a629db5d6b2
Will Johnston <wijohnst@gmail.com>
2023-07-20 21:16:58 -0400

    fixes BlogList render (#50)
    Co-authored-by: Will Johnston <will.johnston@kroger.com>

--- src/stories/HamburgerMenu/HamburgerMenu.style.ts
+++ src/stories/HamburgerMenu/HamburgerMenu.style.ts
@@ -18,9 +18,21 @@ export const SemanticElement = styled.div`
     display: flex;
     flex-direction: column;
     align-items: flex-end;
+    position: absolute;
+    padding: 10px;
   }
 
   .close-button-wrapper {
     width: 25px;
+    position: absolute;
+  }
+
+  .close-button-fill {
+    height: 25px;
+    width: 25px;
+    background-color: ${ColorValuesLightTheme['color-most-prominent']};
+    position: absolute;
+    z-index: -1;
+    border-radius: 10px;
   }
 `;
