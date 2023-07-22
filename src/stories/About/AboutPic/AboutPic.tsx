6d3125c4d3e2fa8fcc85aac8ddc8ddc05f109154
Will Johnston <will.johnston@kroger.com>
2023-07-22 15:33:29 -0400

    Adds boilerplate

---
+++ src/stories/About/AboutPic/AboutPic.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import { SemanticElement } from '@/stories/About/AboutPic/AboutPic.style';
+
+const AboutPic = () => {
+  return (
+    <SemanticElement>
+      <span>AboutPic Works!</span>
+    </SemanticElement>
+  );
+};
+
+export { AboutPic };
